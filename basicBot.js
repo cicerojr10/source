c3db24c915765cdc405fcbf178f8fa1241c25b58
cicerojr10 <cicerojr10@gmail.com>
2017-02-23 20:49:38 -0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -236,7 +236,7 @@
     var botCreatorIDs = ["3851534", "4105209"];
 
     var basicBot = {
-        version: "666 da Rock online está",
+        version: "666 da Rock online esta",
         status: false,
         name: "basicBot",
         loggedInID: null,
